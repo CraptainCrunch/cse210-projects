3f24fb93d4820bcec57a244b69f988ab2ec5cfce
CraptainCrunch <mcmullens.josh@gmail.com>
2025-01-22 14:20:08 -0700

    Completed Prepare 2 Activity

--- prepare/Learning02/Program.cs
+++ prepare/Learning02/Program.cs
@@ -4,6 +4,24 @@ class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello Learning02 World!");
+        Job job1 = new Job();
+        job1._jobTitle = "Documentation Team Lead";
+        job1._company = "BYU-Idaho";
+        job1._startYear = 2020;
+        job1._endYear = 2021;
+
+        Job job2 = new Job();
+        job2._jobTitle = "Helpdesk Tech";
+        job2._company = "StrongMind";
+        job2._startYear = 2022;
+        job2._endYear = 2023;
+
+        Resume myResume = new Resume();
+        myResume._name = "Josh McMullens";
+
+        myResume._jobs.Add(job1);
+        myResume._jobs.Add(job2);
+
+        myResume.Display();
     }
 }
